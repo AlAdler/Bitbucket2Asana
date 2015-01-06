c76114acc7293b9ca768039be78219453473e1a1
Alain Adler <alain.adler@gmail.com>
2015-01-06 00:32:37 +0000

    logs

---
+++ .c9/metadata/preview-https:/bitbucket2asana-aladler.c9.io
@@ -0,0 +1,1 @@
+{"filter":false,"title":"[P] https://bitbucket2asana-aladler.c9.io","undoManager":{"mark":0,"position":-1,"stack":[]},"preview":{"path":"https://bitbucket2asana-aladler.c9.io/comments","previewer":"preview.browser"},"timestamp":1420503987189,"hash":"da39a3ee5e6b4b0d3255bfef95601890afd80709"}
--- app/controllers/commits_controller.rb
+++ app/controllers/commits_controller.rb
@@ -2,13 +2,13 @@ class CommitsController < ApplicationController
     
     def post
         push = JSON.parse params['payload']
-        
+        logger.info push
         push['commits'].each do |commit|
             if (commit['message'].include?('#') || commit['message'].include?('app.asana.com'))
-                logger.debug 'commit has asana'
+                logger.info 'commit has asana'
                 taskid = commit['message'].partition("#").last.partition(" ").first
                 asana_url = "https://app.asana.com/api/1.0/tasks/#{taskid}/stories"
-                logger.debug asana_url
+                logger.info asana_url
                 comment = push['user'] + " pushed to branch " + commit['branch'] + " of " + push['repository']['name'] + "\n- " + commit['message']
                 uri = URI.parse(asana_url)
                 http = Net::HTTP.new(uri.host, uri.port)
@@ -17,9 +17,9 @@ class CommitsController < ApplicationController
                 request = Net::HTTP::Post.new(uri.path, {'Content-Type' =>'application/json'})
                 request.basic_auth(ENV['ASANA_API_KEY'], '')
                 request.body = { "data" => { "text" => comment }}.to_json
-                logger.debug request.body
+                logger.info request.body
                 response = http.request(request)
-                logger.debug response.body
+                logger.info response.body
             end
         end
         
