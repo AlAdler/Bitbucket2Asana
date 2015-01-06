fcdccf2c47eb3a676939a2d29b49276adad0a3a5
Alain Adler <alain.adler@gmail.com>
2015-01-06 01:01:56 +0000

    clean up a bit

--- app/controllers/commits_controller.rb
+++ app/controllers/commits_controller.rb
@@ -6,11 +6,9 @@ class CommitsController < ApplicationController
         push = JSON.parse params['payload']
         logger.info push
         push['commits'].each do |commit|
-            if (commit['message'].include?('#') || commit['message'].include?('app.asana.com'))
-                logger.info 'commit has asana'
+            if (commit['message'].include?('#'))
                 taskid = commit['message'].partition("#").last.partition(" ").first
                 asana_url = "https://app.asana.com/api/1.0/tasks/#{taskid}/stories"
-                logger.info asana_url
                 comment = push['user'] + " pushed to branch " + commit['branch'] + " of " + push['repository']['name'] + "\n- " + commit['message']
                 uri = URI.parse(asana_url)
                 http = Net::HTTP.new(uri.host, uri.port)
@@ -19,12 +17,11 @@ class CommitsController < ApplicationController
                 request = Net::HTTP::Post.new(uri.path, {'Content-Type' =>'application/json'})
                 request.basic_auth(ENV['ASANA_API_KEY'], '')
                 request.body = { "data" => { "text" => comment }}.to_json
-                logger.info request.body
-                response = http.request(request)
-                logger.info response.body
+                http.request(request)
             end
         end
         
         render json: push
     end
+    
 end
